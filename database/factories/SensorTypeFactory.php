dc369219a62cafa75a66e4df0c01ecaf009d5da7
Sergio Eslava <sergio.eslava@nube.aupex.org>
2026-07-08 13:29:37 +0200

    Sensors models added.

---
+++ database/factories/SensorTypeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SensorType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<SensorType>
+ */
+class SensorTypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2026_07_08_111650_create_sensors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('zone_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('sensor_type_id')->constrained()->restrictOnDelete();
+
+            $table->string('name');
+
+            $table->timestamps();
+
+            $table->unique(['zone_id','name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sensors');
+    }
+};
